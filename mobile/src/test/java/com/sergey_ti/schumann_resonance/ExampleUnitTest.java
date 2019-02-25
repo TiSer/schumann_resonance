9f50ee80917292aa8ea4a0f6df7e46c0c0f82048
Sergey TiSer <ug.tiser@gmail.com>
2019-02-25 13:05:37 +0200

    Initial commit

---
+++ mobile/src/test/java/com/sergey_ti/schumann_resonance/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.sergey_ti.schumann_resonance;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
